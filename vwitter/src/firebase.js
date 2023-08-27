e2b4c1a05d3f854552efd15a86d925bb0e01b672
BEANyyy <yevin2001@gmail.com>
2023-08-27 23:11:28 +0900

    firebase.js 서버 생성 후 js 파일 생성

---
+++ vwitter/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBU7w-3VGKnowh0O0TD-BkWE1fZbUZgIwA",
+  authDomain: "vwitter2-61b3a.firebaseapp.com",
+  projectId: "vwitter2-61b3a",
+  storageBucket: "vwitter2-61b3a.appspot.com",
+  messagingSenderId: "336088546660",
+  appId: "1:336088546660:web:c26d6fde3a04ad15760e71",
+  measurementId: "G-DTHEZYE8J5"
+};
+
+export default initializeApp(firebaseConfig);
