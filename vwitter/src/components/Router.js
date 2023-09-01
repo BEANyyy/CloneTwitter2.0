3d56c0887543c2c63364f0bdaf79f4e80c0ea5a7
BEANyyy <yevin2001@gmail.com>
2023-09-02 01:15:45 +0900

    라우터 부분 완료

--- vwitter/src/components/Router.js
+++ vwitter/src/components/Router.js
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom"; // as를 이용해서 HashRouter의 이름을 Router로 바꾸기
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
 
 const AppRouter = () => { // Router를 미리 정의했기 때문에 중복되면 안 되므로 AppRouter로 정의.
-  //Switch : 여러 가지 Route 중 하나만 랜더링 할 수 있게 해줌.
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // setIsLoggedIn : isLoggedIn을 변경할 때 사용하는 함수.
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  // Switch : 여러 가지 Route 중 하나만 랜더링 할 수 있게 해줌.
   return (
     <Router>
       <Switch>
-        <Route />
+        {isLoggedIn ? ( // 삼항연산자로 isLoggedIn의 값에 따라 다른 Route를 보여줌.
+          <Route exact path="/">
+            <Home />
+          </Route>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+          )}
       </Switch> 
     </Router>
   );
